2c7f89d2e89befd628b4e48a06a398d8b87fc476
GeorgiPaunov <georgipaunov84@abv.bg>
2019-09-23 11:24:08 +0300

    Changed the title and the favicon

--- client/src/components/list/list-details/list-item.js
+++ client/src/components/list/list-details/list-item.js
@@ -36,60 +36,12 @@ const ListItem = (props) => {
                         </div>
                     </section>
                     <section className="item-delete">
-                        <button className="btn delete-btn" id="delete" onClick={() => props.removeFromList(props._id)}>Remove book</button>
+                        <button className="btn delete-btn" onClick={() => props.removeFromList(props._id)}>Remove book</button>
                     </section>
                 </section>
             </article>
         </li>
     );
-
-    // return(
-    //     <li className="details-card">
-    //         <section className="book-picture">
-    //             <img src={book.imageUrl} alt="book"/>
-    //         </section>
-    //         <section className="book-info">
-    //             <section className="book-data">
-    //                 <div className="data-row">
-    //                     <h3>title</h3>
-    //                     <h4>{book.title}</h4>
-    //                 </div>
-    //                 <div className="data-row">
-    //                     <h3>grade</h3>
-    //                     <h4>{book.grade}</h4>
-    //                 </div>
-    //                 <div className="data-row">
-    //                     <h3>author</h3>
-    //                     <h4>{book.author}</h4>
-    //                 </div>
-    //                 <div className="data-row">
-    //                     <h3>subject</h3>
-    //                     <h4>{book.subject}</h4>
-    //                 </div>
-    //                 <div className="data-row">
-    //                     <h3>year</h3>
-    //                     <h4>{book.year}</h4>
-    //                 </div>
-    //                 <div className="data-row">
-    //                     <h3>publisher</h3>
-    //                     <h4>{book.publisher}</h4>
-    //                 </div>
-    //                 <div className="data-row">
-    //                     <h3>price</h3>
-    //                     <h4>{book.price.toFixed(2)} lv.</h4>
-    //                 </div>
-    //             </section>
-    //             <section className="book-description">
-    //                 <header className="description-header">
-    //                     <h2>description</h2>
-    //                 </header>
-    //                 <main className="description-main">
-    //                     <p>{book.description}</p>
-    //                 </main>
-    //             </section>
-    //         </section>
-    //     </li>
-    // );
 };
 
 export default ListItem;
